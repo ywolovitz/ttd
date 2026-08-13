43b627b8025dec97efe32fddc3d4add661e9bbb3
Dan-YSW <dan@yswsolutions.co.za>
2026-08-13 14:28:35 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,14 @@
-#COMING SOON
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('Hello from Python on Vercel!'.encode())
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('POST received'.encode())
